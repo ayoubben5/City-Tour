52ca74fdaec9b9a7938157bcdfe34bc553489558
ayoubben5 <60117038+ayoubben5@users.noreply.github.com>
2020-05-20 05:09:15 +0200

    Updates

---
+++ precache-manifest.adf3818f59edb68f2f2414f64f3c122f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b809d6e55523ea48e95f1d9b3f575b97",
+    "url": "/City-Tour/index.html"
+  },
+  {
+    "revision": "37b9218448ab32360f1e",
+    "url": "/City-Tour/static/css/main.75b9cf18.chunk.css"
+  },
+  {
+    "revision": "bcbf70293ab6fce6e3c4",
+    "url": "/City-Tour/static/js/2.a1ce8b1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/City-Tour/static/js/2.a1ce8b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b9218448ab32360f1e",
+    "url": "/City-Tour/static/js/main.6523e95f.chunk.js"
+  },
+  {
+    "revision": "1464eba8f828a2ed54d8",
+    "url": "/City-Tour/static/js/runtime-main.5e90e324.js"
+  },
+  {
+    "revision": "12a6659ff5467c71b599841286fb8e7f",
+    "url": "/City-Tour/static/media/logo.12a6659f.svg"
+  }
+]);
